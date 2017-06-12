a6aea85b60cc44ed51fb148aa5179de98421c8c4
Sonja Duric <duric.sonja@gmail.com>
2017-06-12 16:31:55 -0400

    more setup for knex/pg

---
+++ config.js
@@ -0,0 +1,25 @@
+'use strict';
+
+require('dotenv').config();
+
+const DATABASE_URL = process.env.DATABASE_URL ||
+                       global.DATABASE_URL || 
+                       'postgresql://dev:dev@localhost/dev-pairingsdb';
+
+const TEST_DATABASE_URL = (
+	process.env.TEST_DATABASE_URL ||
+    'postgresql://dev:dev@localhost/test-pairingsdb');
+
+exports.DATABASE = {
+  client: 'pg',
+  connection: DATABASE_URL,
+  // debug: true
+};
+
+exports.TEST_DATABASE = {
+  client: 'pg',
+  connection: TEST_DATABASE_URL,
+};
+
+
+exports.PORT = process.env.PORT || 8080;
