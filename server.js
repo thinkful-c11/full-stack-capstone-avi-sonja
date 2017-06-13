e91026c2790fe8d1799def1755a742cafab3ca27
Avi <avirzajac@gmail.com>
2017-06-13 16:25:44 +0000

    Added in two lines of consts

--- server.js
+++ server.js
@@ -2,5 +2,8 @@
 
 const express = require('express');
 const app = express();
+const {DATABASE,PORT}=require('./config');
+const knex=require('knex')(DATABASE);
+
 app.use(express.static('public'));
 app.listen(process.env.PORT || 8080);
