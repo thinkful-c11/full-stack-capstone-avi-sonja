397c8b4b15b505cb214ccad091c691e33f1e786e
Avi <avirzajac@gmail.com>
2017-06-14 19:37:02 +0000

    Debugged the undefined issue

--- public/js/app.js
+++ public/js/app.js
@@ -12,12 +12,13 @@ function initialisePairsPage(state){
 //RENDER
 function render(state){
   let presentPairingsList='';
-  let htmlDisplay;
+  let htmlDisplay='';
   if(state.showPairingsList===true){
     state.pairingsList.forEach(element => {
-      htmlDisplay += `<div class="show-daily-pairs"><div class="col-6 left-names"><p>${element.name1}</p></div><div class="col-6 right-names"><p>${element.name2}</p></div></div>`
-      //htmlDisplay += `<p>${element.name1} ${element.name2}</p>`
-      console.log("This is partner 1: " + element.name1);
+      //htmlDisplay += `<div class="show-daily-pairs"><div class="col-6 left-names"><p>${element.name1}</p></div><div class="col-6 right-names"><p>${element.name2}</p></div></div>`
+      htmlDisplay += `<p>${element.name1} & ${element.name2}</p>`
+      //console.log("This is partner 1: " + element.name1);
+      //console.log("This is partner 2: " + element.name2);
       console.log("This is the render: " + htmlDisplay);
     }); 
     $('.student-pairings-chart').html(htmlDisplay);
@@ -38,8 +39,8 @@ function getContents(){
     type: 'GET',
     dataType: 'json',
     success: function(json){
-      console.log("These are the pairings: " + json);
-      console.log(json);
+      //console.log("These are the pairings: " + json);
+      //console.log(json);
       appState.pairingsList=json;
       render(appState);
     },
