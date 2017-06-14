9792574886b4c9baca31036e56a878c62e35e4ff
Avi <avirzajac@gmail.com>
2017-06-14 20:41:52 +0000

    Working on next part!

--- public/js/app0.js
+++ public/js/app0.js
@@ -13,15 +13,18 @@ function initialisePairsPage(state){
 function render(state){
   let presentPairingsList='';
   let htmlDisplay='';
+  let dailyExpectedRating=''; //This is the expected overall rating of all pairs that day
   if(state.showPairingsList===true){
     state.pairingsList.forEach(element => {
-      //htmlDisplay += `<div class="show-daily-pairs"><div class="col-6 left-names"><p>${element.name1}</p></div><div class="col-6 right-names"><p>${element.name2}</p></div></div>`
-      htmlDisplay += `<p>${element.name1} & ${element.name2}</p>`
+      htmlDisplay += `<div class="show-admin-chart"><div class="partner-cycle-id">${element.cycles_id}</div><div class="partner-1-id grid">${element.id1}</div><div class="partner-2-id grid">${element.id2}</div><div class="partner-1-name grid">${element.name1}</div><div class="partner-2-name grid">${element.name2}</div><div class="parter-ratings">${element.rating}</div><div class="partner-rating-comments">${element.rating_comment}</div></div>`
+      //htmlDisplay += `<p>Please work!</p>`
       //console.log("This is partner 1: " + element.name1);
       //console.log("This is partner 2: " + element.name2);
-      console.log("This is the render: " + htmlDisplay);
+      //console.log("This is the partner rating: " + element.rating);
+      dailyExpectedRating=`<div class="over-rating-notice"><p>The overall rating for today is: ${element.expected_rating}</p></div>`
+      //console.log("This is the render: " + htmlDisplay);
     }); 
-    $('.student-pairings-chart').html(htmlDisplay);
+    $('.admin-dashboard-chart').html(htmlDisplay);
         //state.showPairingsList
   }
   else{
@@ -40,7 +43,7 @@ function getContents(){
     dataType: 'json',
     success: function(json){
       //console.log("These are the pairings: " + json);
-      //console.log(json);
+      console.log(json);
       appState.pairingsList=json;
       render(appState);
     },
