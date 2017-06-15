1286c5cee39bc4fd70c10e53a294a489e828d6bb
Avi <avirzajac@gmail.com>
2017-06-15 19:55:27 +0000

    Adding users now works

--- public/js/userapp.js
+++ public/js/userapp.js
@@ -16,12 +16,6 @@ function render(state){
     let dailyExpectedRating=''; //This is the expected overall rating of all pairs that day
     if(state.showPairingsList===true){
         state.pairingsList.forEach(element => {
-//             CREATE TABLE cohort_members(
-                // id serial PRIMARY KEY,
-                // first_name TEXT NOT NULL,
-                // last_name TEXT NOT NULL,
-                // cohort_id int NOT NULL,
-                // location TEXT);
             htmlDisplay += `<div class="show-admin-chart"><div class="user-user-id grid">${element.id}</div><div class="user-name-first grid">${element.first_name}</div><div class="user-name-last grid">${element.last_name}</div><div class="user-cohort-id grid">${element.cohort_id}</div><div class="user-user-location grid">${element.location}</div></div>`
             //htmlDisplay += `<p>Please work!</p>`
             //console.log("This is partner 1: " + element.name1);
@@ -39,7 +33,48 @@ function render(state){
 }
 //EVENT HANDLERS
 function eventHandlers(){
-  render(appState);
+    $('.create-add-user-form').submit(function(event) {
+        event.preventDefault();
+        const first_name = $("#user-first-name").val();
+        const last_name=$("#user-last-name").val();
+        const cohort_id=$("#user-cohort-id").val();
+        const location=$("#user-location").val();
+        const data = {first_name, last_name, cohort_id, location};
+        console.log(data);
+        // $.ajax({
+        //     type: 'POST',
+        //     $("#yada-yada").val()
+        //     const first_name = $("#user-first-name").val()
+        // })
+        $.ajax({
+            url: '/cohort_members',
+            dataType: 'json',
+            type: 'post',
+            contentType: 'application/json',
+            data: JSON.stringify(data),
+            success: function(json){
+                //console.log(json);
+            },
+        });
+    });
+    $('.create-update-user-form').submit(function(event) {
+        event.preventDefault();
+        const id = $("#student-user-id").val();
+        const location=$("#student-user-location").val();
+        const data = {id, location};
+        console.log(data);
+        $.ajax({
+            url: `/cohort_members/${id}`,
+            dataType: 'json',
+            type: 'put',
+            contentType: 'application/json',
+            data: JSON.stringify(data),
+            success: function(json){
+                console.log(json);
+            },
+        });
+    });
+    render(appState);
 }
 //localhost:8080/cohort_members
 function getContents(){
@@ -49,7 +84,7 @@ function getContents(){
     dataType: 'json',
     success: function(json){
       //console.log("These are the pairings: " + json);
-      console.log(json);
+      //console.log(json);
       appState.pairingsList=json;
       render(appState);
     },
