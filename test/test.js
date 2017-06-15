4ec252250feb4ec4ee44f154cc89a49841a6e21d
Sonja Duric <duric.sonja@gmail.com>
2017-06-15 12:44:23 -0400

    more attempts at testing

--- test/test.js
+++ test/test.js
@@ -3,7 +3,13 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 //const faker = require('faker');
-const{TEST_DATABASE, PORT, TEST_DATABASE_URL}=require('../config');
+const{PORT, TEST_DATABASE_URL} = require('../config');
+
+const TEST_DATABASE = {
+  client: 'pg',
+  connection: TEST_DATABASE_URL,
+};
+
 const knex = require('knex')(TEST_DATABASE);
 console.log(TEST_DATABASE_URL);
 
