7aa8945dddfa4ee38c7c629ee2e164646c1709ea
Avi <avirzajac@gmail.com>
2017-06-30 13:59:06 +0000

    Removed console.logs from files from testing

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,6 @@ function render(state){
   let htmlDisplay='';
   if(state.showPairingsList===true){
     state.pairingsList.forEach(element => {
-      //htmlDisplay += `<div class="show-daily-pairs"><div class="col-6 left-names">`; //<p>${element.name1}</p></div><div class="col-6 right-names"><p>${element.name2}</p></div></div>`
       htmlDisplay += `<p>${element.name1} `;
       if (element.name2) {
         htmlDisplay += `& ${element.name2}</p>`;
@@ -24,12 +23,8 @@ function render(state){
       else{
         htmlDisplay += `</p>`; //</div></div>`;
       }
-      //console.log("This is partner 1: " + element.name1);
-      //console.log("This is partner 2: " + element.name2);
-      console.log("This is the render: " + htmlDisplay);
     }); 
     $('.student-pairings-chart').html(htmlDisplay);
-        //state.showPairingsList
   }
   else{
     presentPairingsList+='<p>No pairings</p>';
@@ -37,14 +32,11 @@ function render(state){
 }
 //EVENT HANDLERS
 function eventHandlers(){
-
   $('.rotate').on('click', function(event){
-
     (appState.id >= 6) ? appState.id=1: appState.id++;
     getContentsByID(appState.id);
     render(appState);
   });
-
   render(appState);
 }
 //localhost:8080/cohort_members
@@ -54,30 +46,23 @@ function getContents(){
     type: 'GET',
     dataType: 'json',
     success: function(json){
-      //console.log("These are the pairings: " + json);
-      //console.log(json);
       appState.pairingsList=json;
       render(appState);
     },
   });
 }
-
-
 function getContentsByID(id){
   $.ajax({
     url: `/admin/todays_pairs/${id}`,
     type: 'GET',
     dataType: 'json',
     success: function(json){
-      //console.log("These are the pairings: " + json);
-      //console.log(json);
       appState.pairingsList=json;
       render(appState);
     },
         
   });
 }
-
 //RUN THIS CUTENESS
 $(function(){
   eventHandlers();
--- public/js/userapp.js
+++ public/js/userapp.js
@@ -1,5 +1,4 @@
 'use strict';
-
 //APP INITIALISES
 const appState={
   pairingsList:[],
@@ -17,13 +16,10 @@ function render(state){
   if(state.showPairingsList===true){
     state.pairingsList.forEach(element => {
       if(element.active===true){
-            //<div class="grid remove-student-option"><span class="X">X</span>
         htmlDisplay += `<div class="show-admin-chart"><div class="grid remove-student-option"><span class="X"><button data-rabbits="${element.id}" class="remove-student-row" type="button">☒</button></span></div><div class="user-user-id grid">${element.id}</div><div class="user-name-first grid">${element.first_name}</div><div class="user-name-last grid">${element.last_name}</div><div class="user-cohort-id grid">${element.cohort_id}</div><div class="user-user-location grid">${element.location}</div></div>`;
       }
-           
     }); 
     $('.concept-users-chart').html(htmlDisplay);
-      
   }
   else{
     presentPairingsList+='<p>No pairings</p>';
@@ -38,8 +34,6 @@ function eventHandlers(){
     const cohort_id=$('#user-cohort-id').val();
     const location=$('#user-location').val();
     const data = {first_name, last_name, cohort_id, location};
-    console.log(data);
-
     $.ajax({
       url: '/cohort_members',
       dataType: 'json',
@@ -47,9 +41,7 @@ function eventHandlers(){
       contentType: 'application/json',
       data: JSON.stringify(data),
       success: function(json){
-        console.log(json);
         appState.pairingsList.push(json[0]);
-        console.log(appState);
         render(appState);
       },
     });
@@ -60,7 +52,6 @@ function eventHandlers(){
     const id = $('#student-user-id').val();
     const location=$('#student-user-location').val();
     const data = {id, location};
-    console.log(data);
     $.ajax({
       url: `/cohort_members/${id}`,
       dataType: 'json',
@@ -72,20 +63,14 @@ function eventHandlers(){
       },
     });
     appState.pairingsList.forEach(element=>{
-      console.log(element.id);
-      console.log(data.id);
       if(element.id==data.id){
         element.location=data.location;
-        console.log(element);
       }
     });
-    console.log(appState);
     render(appState);
   });
   $('.concept-users-chart').on('click','.remove-student-row', function(event){
     event.preventDefault();
-    console.log(event);
-    console.log($(this).data('rabbits'));
     let rabbits=$(this).data('rabbits');
     $.ajax({
       url: `/cohort_members/${rabbits}`,
@@ -95,14 +80,12 @@ function eventHandlers(){
         console.log(json);
       }
     });
-    console.log(appState);
     appState.pairingsList.forEach(element =>{
       if(element.id===rabbits){
         element.active=false;
       }
     });
     render(appState);
-        //console.log(appState);
   });
   render(appState);
 }
