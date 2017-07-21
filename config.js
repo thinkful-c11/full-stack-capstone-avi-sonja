388394d6d922c3680b94b5d7eea766436519aa54
Avi <avirzajac@gmail.com>
2017-07-21 11:23:57 -0400

    Updated config.js files simplifying

--- config.js
+++ config.js
@@ -1,27 +1,21 @@
-'use strict';
-
-if (process.env.NODE_ENV !== 'production'){
-  require('dotenv').config();
-}
-
-const DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL || 
-                       'postgresql://dev:dev@localhost/dev-pairingsdb';
-
-const TEST_DATABASE_URL = (
-	process.env.TEST_DATABASE_URL ||
-    'postgresql://dev:dev@localhost/test-pairingsdb');
-
+// 'use strict';
+// if (process.env.NODE_ENV !== 'production'){
+//   require('dotenv').config();
+// }
+require('dotenv').config({silent:true});
+// const DATABASE_URL = process.env.DATABASE_URL ||
+//                        global.DATABASE_URL || 
+//                        'postgresql://dev:dev@localhost/dev-pairingsdb';
+// const TEST_DATABASE_URL = (
+// 	process.env.TEST_DATABASE_URL ||
+//     'postgresql://dev:dev@localhost/test-pairingsdb');
 exports.DATABASE = {
   client: 'pg',
-  connection: DATABASE_URL,
+  connection: process.env.DATABASE_URL || global.DATABASE_URL || 'postgresql://dev:dev@localhost/dev-pairingsdb',
   // debug: true
 };
-
 exports.TEST_DATABASE = {
   client: 'pg',
-  connection: TEST_DATABASE_URL,
+  connection: process.env.TEST_DATABASE_URL || 'postgresql://dev:dev@localhost/test-pairingsdb',
 };
-
-
 exports.PORT = process.env.PORT || 8080;
