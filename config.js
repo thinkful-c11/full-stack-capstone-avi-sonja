baeb64ba3b7b752f42e0d161ee571b5aefe04108
Avi <avirzajac@gmail.com>
2017-07-21 19:25:28 -0400

    Revert "Updated config.js files simplifying"
    This reverts commit 388394d6d922c3680b94b5d7eea766436519aa54.

--- config.js
+++ config.js
@@ -1,21 +1,27 @@
-// 'use strict';
-// if (process.env.NODE_ENV !== 'production'){
-//   require('dotenv').config();
-// }
-require('dotenv').config({silent:true});
-// const DATABASE_URL = process.env.DATABASE_URL ||
-//                        global.DATABASE_URL || 
-//                        'postgresql://dev:dev@localhost/dev-pairingsdb';
-// const TEST_DATABASE_URL = (
-// 	process.env.TEST_DATABASE_URL ||
-//     'postgresql://dev:dev@localhost/test-pairingsdb');
+'use strict';
+
+if (process.env.NODE_ENV !== 'production'){
+  require('dotenv').config();
+}
+
+const DATABASE_URL = process.env.DATABASE_URL ||
+                       global.DATABASE_URL || 
+                       'postgresql://dev:dev@localhost/dev-pairingsdb';
+
+const TEST_DATABASE_URL = (
+	process.env.TEST_DATABASE_URL ||
+    'postgresql://dev:dev@localhost/test-pairingsdb');
+
 exports.DATABASE = {
   client: 'pg',
-  connection: process.env.DATABASE_URL || global.DATABASE_URL || 'postgresql://dev:dev@localhost/dev-pairingsdb',
+  connection: DATABASE_URL,
   // debug: true
 };
+
 exports.TEST_DATABASE = {
   client: 'pg',
-  connection: process.env.TEST_DATABASE_URL || 'postgresql://dev:dev@localhost/test-pairingsdb',
+  connection: TEST_DATABASE_URL,
 };
+
+
 exports.PORT = process.env.PORT || 8080;
