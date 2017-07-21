117942defc9d9887f23fcda7f1f709bb59b87b7f
Avi <avirzajac@gmail.com>
2017-07-21 11:24:49 -0400

    Removed former consts for the connection

--- config.js
+++ config.js
@@ -1,14 +1,4 @@
-// 'use strict';
-// if (process.env.NODE_ENV !== 'production'){
-//   require('dotenv').config();
-// }
 require('dotenv').config({silent:true});
-// const DATABASE_URL = process.env.DATABASE_URL ||
-//                        global.DATABASE_URL || 
-//                        'postgresql://dev:dev@localhost/dev-pairingsdb';
-// const TEST_DATABASE_URL = (
-// 	process.env.TEST_DATABASE_URL ||
-//     'postgresql://dev:dev@localhost/test-pairingsdb');
 exports.DATABASE = {
   client: 'pg',
   connection: process.env.DATABASE_URL || global.DATABASE_URL || 'postgresql://dev:dev@localhost/dev-pairingsdb',
